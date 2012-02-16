80a6d374716b6a56e990695bb4a8685fcd0fd826
Jack Poulson <jack.poulson@gmail.com>
2012-02-16 05:42:41 +0000

    Updating BG/P toolchain file.

--- cmake/toolchains/BGP-xl_aug2011-essl_4.4.1-1.cmake
+++ cmake/toolchains/BGP-xl_aug2011-essl_4.4.1-1.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_NAME BlueGeneP-static)
 
 # The serial XL compilers
-set(CMAKE_C_COMPILER       /soft/apps/ibmcmp-aug2011/vacpp/bg/9.0/bin/bgxlc_r)
-set(CMAKE_CXX_COMPILER     /soft/apps/ibmcmp-aug2011/vacpp/bg/9.0/bin/bgxlC_r)
+set(CMAKE_C_COMPILER   /soft/apps/ibmcmp-aug2011/vacpp/bg/9.0/bin/bgxlc_r)
+set(CMAKE_CXX_COMPILER /soft/apps/ibmcmp-aug2011/vacpp/bg/9.0/bin/bgxlC_r)
 
 # The MPI wrappers for the XL C and C++ compilers
 set(MPI_C_COMPILER   /bgsys/drivers/ppcfloor/comm/bin/mpixlc_r)
@@ -11,7 +11,7 @@ set(MPI_CXX_COMPILER /bgsys/drivers/ppcfloor/comm/bin/mpixlcxx_r)
 set(CXX_FLAGS "-g -O4")
 
 set(LAPACK "-L/soft/apps/LAPACK -llapack_bgp")
-set(ESSL_DIR "/soft/apps/ESSL-4.3.1-1")
+set(ESSL_DIR "/soft/apps/ESSL-4.4.1-1")
 set(IBMCMP_DIR "/soft/apps/ibmcmp-aug2011")
 set(XLF_DIR "${IBMCMP_DIR}/xlf/bg/11.1/bglib")
 set(XLSMP_DIR "${IBMCMP_DIR}/xlsmp/bg/1.7/bglib")
